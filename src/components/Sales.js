b02c72debba46182c0d7b6fd3c0d5a9e34444318
fekkai <jkim@fekkai.com>
2020-03-29 14:36:01 -0400

    add favicon

--- src/components/Sales.js
+++ src/components/Sales.js
@@ -1,199 +1,165 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-//components
-import Color from './Color'
-import Conditions from './Conditions'
-import Goals from './Goals'
-import { Row } from './common'
-import { Link } from 'react-router-dom'
-import Fade from 'react-reveal/Fade'
-import { Paper } from '@material-ui/core'
-import { ClipLoader, PulseLoader } from 'react-spinners'
-import CircularProgress from '@material-ui/core/CircularProgress'
+// components
+import { Link } from "react-router-dom";
+import { ClipLoader, PulseLoader } from "react-spinners";
 
 // styling
-import '../styles/Panel.scss'
-import { css } from '@emotion/core'
+import "../styles/Panel.scss";
+import { css } from "@emotion/core";
 
-// react CSV
-import { CSVLink, CSVDownload } from 'react-csv'
-
-import axios from 'axios'
-import aws4 from 'aws4'
-
-const override = css`
-  display: block;
-  margin: 0 auto;
-  margin-top: ${7}%;
-`
+// dependencies
+import axios from "axios";
+import { CSVLink } from "react-csv";
 
 export default class Sales extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      reload: false,
-      filter: '',
       data: [],
-      ascending: false,
       loading: true,
-      checked: false,
-      colorOpen: false,
-      conditionOpen: false,
-      goalsOpen: false
-    }
+      completedQuizCount: "",
+      totalQuizCount: "",
+      abandonedQuiz: ""
+    };
   }
 
   async componentDidMount() {
-    this.fetchOrders()
-    await this.fetchQuizData()
-    await this.findSales()
+    await this.fetchOrders();
+    await this.fetchQuizData();
+    await this.findOrders();
     await this.setState({
       loading: false
-    })
+    });
   }
 
   fetchOrders = async () => {
     try {
-      let response = await axios(
-        'https://bespoke-backend.herokuapp.com/fekkai'
-      )
       let responses = await axios(
-        'https://bespoke-backend.herokuapp.com/orders'
-      )
-      console.log(responses.data)
-
-      // const orders = response.data.orders;
-      const orders = responses.data
-      this.setState({ orders })
+        "https://bespoke-backend.herokuapp.com/orders"
+      );
+      const orders = responses.data;
+      this.setState({ orders });
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
   fetchQuizData = async () => {
     try {
+      // query user codes api
       let response = await axios(
         `https://bespoke-backend.herokuapp.com/fekkai-backend`
-      )
-      // response = JSON.parse(JSON.stringify(response));
-      const emails = []
-      let completedQuizCount = 0
-      let abandonedQuiz = 0
-      let totalQuizCount = response.data.length
+      );
+      const emails = [];
+      let completedQuizCount = 0;
+      let abandonedQuiz = 0;
+      let totalQuizCount = response.data.length;
 
-      for (let userCode of response.data
-        // .slice(response.data.length - 100, response.data.length)
-        .reverse()) {
+      // query user data instances w/ user_code
+      for (let userCode of response.data.reverse()) {
         let userResponse = await axios.get(
           `https://fekkai-backend.herokuapp.com/backend/formula?user_code=${userCode.user_code}`
-        )
+        );
 
-        if (
-          // userResponse.data.created > "2020-03-19T23:59:59" &&
-          userResponse.data.user_data.compute === false
-        ) {
-          abandonedQuiz++
+        // increment abandoned quiz instance if quiz compute is false
+        if (userResponse.data.user_data.compute === false) {
+          abandonedQuiz++;
 
           this.setState({
             abandonedQuiz
-          })
+          });
         }
 
-        if (
-          // userResponse.data.created > "2020-03-19T23:59:59" &&
-          userResponse.data.user_data.compute === true
-        ) {
-          // console.log(userResponse.data)
+        // increment abandoned quiz instance if quiz compute is true
+        if (userResponse.data.user_data.compute === true) {
+          // avoid pushing duplicate emails in emails array
           if (
             !emails.includes(
               userResponse.data.user_data.email.toLocaleLowerCase()
             )
           ) {
-            emails.push(userResponse.data.user_data.email.toLocaleLowerCase())
+            emails.push(userResponse.data.user_data.email.toLocaleLowerCase());
           }
-
-          completedQuizCount++
+          completedQuizCount++;
 
           this.setState({
             completedQuizCount
-          })
+          });
         }
       }
       this.setState({
         emails,
         totalQuizCount
-      })
+      });
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
-  findSales = () => {
-    let saleEmail = 0
-    let nonSaleEmail = 0
-    let totalSales = 0
-    let csv = [['created_at', 'email', 'subtotal', 'total']]
+  findOrders = () => {
+    let totalSales = 0;
+    let csv = [["created_at", "email", "subtotal", "total"]];
     // emails compute true
-    console.log(this.state.emails)
     for (let email of this.state.emails) {
       for (let order of this.state.orders) {
         if (order.total && email === order.email.toLocaleLowerCase()) {
-          totalSales += parseFloat(order.total)
-          this.setState({ totalSales })
-          csv.push([order.created_at, order.email, order.subtotal, order.total])
-          // console.log(
-          //   order.created_at,
-          //   order.email,
-          //   order.subtotal_price,
-          //   order.total_price
-          // );
+          totalSales += parseFloat(order.total);
+          this.setState({ totalSales });
+          csv.push([
+            order.created_at,
+            order.email,
+            order.subtotal,
+            order.total
+          ]);
         }
       }
-      this.setState({ totalSales, csv })
+      this.setState({ totalSales, csv });
     }
-  }
+  };
 
   render() {
-    let counter = 0
-    const { filter, data, ascending } = this.state
-    const filteredData = data.filter(item => {
-      return Object.keys(item).some(key =>
-        key === 'condition' || key === 'hairGoals' || key === 'hairColor'
-          ? item[key]
-              .toString()
-              .toLocaleLowerCase()
-              .includes(filter.toLocaleLowerCase())
-          : ''
-      )
-    })
-
+    const {
+      loading,
+      completedQuizCount,
+      totalQuizCount,
+      abandonedQuiz
+    } = this.state;
     return (
       <div className="dashboard">
         <span>
           <Link to="/stylist-panel-list">
-            <button id="list-view-btn">← LIST VIEW</button>
+            <button id="list-view-btn">← LIST</button>
           </Link>
           <br />
           <br />
-          TOTAL QUIZ COUNT: {this.state.loading? <ClipLoader size={6} /> :this.state.totalQuizCount}
-          <br /> COMPLETED QUIZ COUNT: {this.state.completedQuizCount}
-          <br /> ABANDONED QUIZ COUNT: {this.state.abandonedQuiz}
+          TOTAL QUIZ COUNT: {loading ? <ClipLoader size={6} /> : totalQuizCount}
+          <br /> COMPLETED QUIZ COUNT: {completedQuizCount}&nbsp;
+          {!loading
+            ? "(" +
+              ((completedQuizCount / totalQuizCount) * 100).toFixed(2) +
+              " %)"
+            : ""}
+          <br /> ABANDONED QUIZ COUNT: {abandonedQuiz}&nbsp;
+          {!loading
+            ? "(" + ((abandonedQuiz / totalQuizCount) * 100).toFixed(2) + " %)"
+            : ""}
           {/* <br /> COMPUTE NULL: {this.state.computeNull} */}
           <br /> <br />
-          <span style={{ display: 'flex', flexDirection: 'row' }}>
-            TOTAL SALES:{' '}
+          <span style={{ display: "flex", flexDirection: "row" }}>
+            TOTAL SALES:{" "}
             {this.state.loading === false ? (
               <span>
-                {this.state.totalSales}{' '}
+                {this.state.totalSales}{" "}
                 <CSVLink data={this.state.csv}>DOWNLOAD CSV</CSVLink>
               </span>
             ) : (
               <span
                 style={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  marginLeft: '5px'
+                  display: "flex",
+                  flexDirection: "row",
+                  marginLeft: "5px"
                 }}
               >
                 COMPILING CSV <PulseLoader size={6} />
@@ -202,6 +168,6 @@ export default class Sales extends Component {
           </span>
         </span>
       </div>
-    )
+    );
   }
 }
